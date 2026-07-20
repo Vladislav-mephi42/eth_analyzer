0ed0ea662618bb4896ea0f4ef207c9fb092d377d
klianchenkov.va <klianchenkov.va@dozen.mephi.ru>
2026-07-20 21:08:43 +0300

    feat(interprocessor-bridge): add C++ socket class

---
+++ src/analysis_engine/sockets.h
@@ -0,0 +1,165 @@
+#include <arpa/inet.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <nlohmann/json.hpp>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+using json = nlohmann::json;
+
+class Socket {
+protected:
+  int fd;
+
+public:
+  Socket(int domain, int type, int protocol) {
+    fd = socket(domain, type, protocol);
+  }
+  Socket(int fd_in) { fd = fd_in; }
+  ~Socket() {
+    if (fd != -1) {
+      close(fd);
+    }
+  }
+  Socket(const Socket &other) = delete;
+  Socket &operator=(const Socket &other) = delete;
+
+  Socket(Socket &&other) {
+    fd = other.fd;
+    other.fd = -1;
+  }
+  Socket &operator=(Socket &&other) {
+    std::swap(fd, other.fd);
+    return *this;
+  }
+  void send_n(const char *buf, int len, int flags = 0) {
+    int current = 0;
+    int total = 0;
+
+    while (current != len) {
+      total = send(fd, buf + current, len - current, flags);
+      if (total < 0) {
+        throw std::system_error(errno, std::generic_category(), "bad send");
+      }
+      current += total;
+    }
+  }
+  void recv_n(char *buf, int len, int flags = 0) {
+    int current = 0;
+    int total = 0;
+
+    while (current != len) {
+      total = recv(fd, (void *)(buf + current), len - current, flags);
+      if (total < 0) {
+        throw std::system_error(errno, std::generic_category(), "bad send");
+      }
+      if (total == 0) {
+        throw std::runtime_error("recv: connection closed by peer");
+      }
+      current += total;
+    }
+  }
+  void auto_send(const char *buf, int len, int flags = 0) {
+    uint32_t inet_len = htonl((uint32_t)len);
+    int res = send(fd, &inet_len, sizeof(inet_len), 0);
+    if (res <= 0) {
+      throw std::system_error(errno, std::generic_category(), "bad send");
+    }
+    send_n(buf, len, flags);
+  }
+  int auto_recv(char *buf, int len, int flags = 0) {
+    uint32_t inet_len;
+    int res = recv(fd, &inet_len, sizeof(inet_len), 0);
+    if (res <= 0) {
+      throw std::system_error(errno, std::generic_category(), "bad recv");
+    }
+    inet_len = ntohl(inet_len);
+    if (inet_len > len) {
+      throw std::runtime_error("inet len is bigger than can be");
+    }
+    recv_n(buf, (int)inet_len, flags);
+    return (int)inet_len;
+  }
+
+  void send_json(const json &data) {
+    auto ser = data.dump();
+
+    auto_send(ser.c_str(), ser.size());
+  }
+  json recv_json() {
+    uint32_t inet_len;
+    int res = recv(fd, &inet_len, sizeof(inet_len), 0);
+    if (res <= 0) {
+      throw std::system_error(errno, std::generic_category(), "bad recv");
+    }
+    int len = ntohl(inet_len);
+    std::string buf;
+    buf.resize(len);
+    recv_n(buf.data(), len);
+    return json::parse(buf);
+  }
+};
+
+class Client_socket : public Socket {
+
+public:
+  Client_socket(int domain, int type, int protocol)
+      : Socket(domain, type, protocol) {}
+  Client_socket(int fd) : Socket(fd) {}
+  ~Client_socket() {}
+
+  void connect(const sockaddr *addr, socklen_t addrlen) {
+    if (::connect(fd, addr, addrlen) == -1) {
+      throw std::system_error(errno, std::generic_category(), "connect failed");
+    }
+  }
+  void connect(const char *server_ip, uint16_t port) {
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(server_ip);
+    if (::connect(fd, reinterpret_cast<const sockaddr *>(&addr),
+                  sizeof(addr)) == -1) {
+      throw std::system_error(errno, std::generic_category(), "connect failed");
+    }
+  }
+};
+
+class Server_socket : public Socket {
+
+public:
+  Server_socket(int domain, int type, int protocol)
+      : Socket(domain, type, protocol) {}
+  ~Server_socket() {}
+
+  void bind(const sockaddr *addr, socklen_t addrlen) {
+    if (::bind(fd, addr, addrlen) == -1) {
+      throw std::system_error(errno, std::generic_category(), "bind failed");
+    }
+  }
+  void bind(uint16_t port) {
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    if (::bind(fd, reinterpret_cast<const sockaddr *>(&addr), sizeof(addr)) ==
+        -1) {
+      throw std::system_error(errno, std::generic_category(), "bind failed");
+    }
+  }
+  void listen(int backlog) {
+    if (backlog > SOMAXCONN) {
+      throw std::logic_error("max backlog has been exceeded");
+    }
+    if (::listen(fd, backlog) == -1) {
+      throw std::system_error(errno, std::generic_category(), "listen failed");
+    }
+  }
+  Client_socket accept() {
+    int client = ::accept(fd, NULL, NULL);
+    if (client == -1) {
+      throw std::system_error(errno, std::generic_category(), "bad_accept");
+    }
+    return Client_socket(client);
+  }
+};
