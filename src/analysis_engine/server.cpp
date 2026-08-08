83b604c0d270fe85c32da1de7f1c5f8eaf0809c0
klianchenkov.va <klianchenkov.va@dozen.mephi.ru>
2026-08-08 13:07:06 +0300

    logging(cpp server): change server.cpp

--- src/analysis_engine/server.cpp
+++ src/analysis_engine/server.cpp
@@ -1,15 +1,20 @@
 #include "graph/graph.h"
 #include "sockets.h"
+#include <cstdlib>
 #include <cstring>
 #include <nlohmann/json.hpp>
 #include <signal.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <thread>
-
 void sigchld_handler(int signum) {
   while (waitpid(-1, NULL, WNOHANG) > 0) {
   }
+}
+
+void sigint_handler(int signum) {
+  std::cout << "\n[INFO] Finish work ..................." << std::endl;
+  exit(0);
 }
 
 void client_handle(Socket &socket) {
@@ -20,7 +25,8 @@ void client_handle(Socket &socket) {
     json data = socket.recv_json();
     if (data.contains("end")) {
       bool res = graph.is_cycled(start_address);
-      std::cout << "RESULT OF CHECK ==== " << res << std::endl;
+      std::cout << "[INFO] counter: " << counter << std::endl;
+      std::cout << "[INFO] result of check: " << res << std::endl;
       break;
     } else {
       graph.add(data);
@@ -28,34 +34,50 @@ void client_handle(Socket &socket) {
       if (counter == 1) {
         start_address = data["target_node"]["address"];
       }
-      std::cout << counter << std::endl;
     }
   }
 }
 
 int main() {
-  struct sigaction action_chld;
-  memset(&action_chld, 0, sizeof(struct sigaction));
-  action_chld.sa_flags = SA_RESTART;
-  action_chld.sa_handler = sigchld_handler;
-  sigaction(SIGCHLD, &action_chld, NULL);
+  try {
+    int port = 7009;
+    int backlog = 8;
 
-  Server_socket server(AF_INET, SOCK_STREAM, 0);
-  server.bind(7009);
-  server.listen(8);
-  pid_t pid;
-  while (true) {
-    auto socket = server.accept();
-    pid = fork();
-    if (pid == 0) {
-      server.close();
-      client_handle(socket);
+    struct sigaction action_chld;
+    memset(&action_chld, 0, sizeof(struct sigaction));
+    action_chld.sa_flags = SA_RESTART;
+    action_chld.sa_handler = sigchld_handler;
+    sigaction(SIGCHLD, &action_chld, NULL);
+
+    struct sigaction action_sigint;
+    memset(&action_sigint, 0, sizeof(struct sigaction));
+    action_sigint.sa_flags = SA_RESTART;
+    action_sigint.sa_handler = sigint_handler;
+    sigaction(SIGINT, &action_sigint, NULL);
+
+    Server_socket server(AF_INET, SOCK_STREAM, 0);
+
+    server.bind(port);
+    std::cout << "[INFO] bind to port " << port << std::endl;
+    server.listen(backlog);
+    std::cout << "[INFO] start listening with backlog " << backlog << std::endl;
+    pid_t pid;
+    while (true) {
+      auto socket = server.accept();
+      std::cout << "[INFO] accept client" << std::endl;
+      pid = fork();
+      if (pid == 0) {
+        server.close();
+        client_handle(socket);
+        socket.close();
+        break;
+      }
+      if (pid == 0) {
+        break;
+      }
       socket.close();
-      break;
     }
-    if (pid == 0) {
-      break;
-    }
-    socket.close();
+  } catch (const std::exception &e) {
+    std::cout << "[ERROR] " << e.what() << std::endl;
   }
 }
